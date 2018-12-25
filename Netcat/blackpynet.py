eca1b655f4595fe364050a2a9220b988d14d5966
CHEAB <aa224iu@student.lnu.se>
2018-12-25 16:57:46 +0100

    added interface for -h

--- Netcat/blackpynet.py
+++ Netcat/blackpynet.py
@@ -12,3 +12,30 @@ execute            = ""
 target             = ""
 upload_destination = ""
 port               = 0
+
+def usage():
+   print "bLACKPY Net Tool\n\n"
+   
+
+   print "Usage: blackpypnet.py -t target_host -p port"
+   print "-l --listen              - listen on [host]:[port] for ", \
+         "incoming connections"
+   print "-e --execute=file_to_run - execute the given file upon ", \
+         "receiving a connection"
+   print "-c --command             - initialize a command shell"
+   print "-u --upload=destination  - upon receiving connection upload a ", \
+         "file and write to [destination]\n\n"
+
+
+   print "Examples:\nblackpypnet.py -t 192.168.0.1 -p 5555 -l -c\n", \
+         "blackpypnet.py -t 192.168.0.1 -p 5555 -l -c\n", \
+         "blackpypnet.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe\n", \
+         "blackpypnet.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\"\n", \
+         "echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.11.12 -p 135"
+   sys.exit(0)
+
+#main
+def main():
+   usage()
+
+main()
