f7090727cd5488cd41e3dc3f9691088dbfd04e7c
CHEAB <aa224iu@student.lnu.se>
2018-12-25 17:15:02 +0100

    -h or -help working

--- Netcat/blackpynet.py
+++ Netcat/blackpynet.py
@@ -36,6 +36,25 @@ def usage():
 
 #main
 def main():
-   usage()
+   global listen
+   global port
+   global execute
+   global command
+   global upload_destination
+   global target
 
+   if not len(sys.argv[1:]):
+      usage()
+
+   #read the command line option
+   try:
+      opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu", \
+                  ["help","listen","execute","target","port","command","upload"])
+   except getopt.GetoptError as err:
+      print str(err)
+      usage()
+   for o,a in opts:
+      if o in ("-h","-help"):
+         usage()
+      
 main()
